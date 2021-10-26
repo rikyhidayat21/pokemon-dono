5b693e4a3fcb882b521049f07aa43f2feb03ffb1
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-10-26 10:16:39 +0700

    add styled Badge

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -1,5 +1,18 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+
+const Badge = styled.div`
+  background-color: violet;
+  color: white;
+  text-decoration: none;
+  padding: 3px 6px;
+  position: relative;
+  display: inline-block;
+  border-radius: 12px;
+  margin: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+`
 
 export default function PokemonDetail({ pokemon }) {
   return (
@@ -9,17 +22,16 @@ export default function PokemonDetail({ pokemon }) {
 
       <Row>
         <Col md={6}>
-          <ul>
-            {pokemon &&
-              pokemon.moves.map((move, index) => (
-                <li key={index}>{move.move.name}</li>
-              ))}
-          </ul>
+          {pokemon &&
+            pokemon.moves.map((move, index) => (
+              <Badge key={index}>{move.move.name}</Badge>
+            ))}
         </Col>
         <Col md={6}>
           {pokemon &&
             pokemon.types.map((type, index) => (
-              <li key={index}>{type.type.name}</li>
+              // <li key={index}>{type.type.name}</li>
+              <Badge key={index}>{type.type.name}</Badge>
             ))}
         </Col>
       </Row>
