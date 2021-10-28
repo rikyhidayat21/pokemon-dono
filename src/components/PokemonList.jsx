7f1fa04e86e7dbae5391b614ed888cf66d420f83
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-10-28 19:32:11 +0700

    fix bugs clash between useEffect and useQuery

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard.jsx";
 import { Row } from "react-bootstrap";
 
 export default function PokemonList({ pokemons }) {
+  const [poke, setPoke] = useState([]);
+
+  useEffect(() => {
+    let getPokemons;
+
+    if (JSON.parse(localStorage.getItem("pokemonList"))) {
+      getPokemons = JSON.parse(localStorage.getItem("pokemonList"));
+    } else {
+      getPokemons = pokemons || [];
+    }
+
+    const updatePokemon = getPokemons.map((poke) => {
+      return {
+        ...poke,
+        owned: poke.owned ? poke.owned : 0,
+      };
+    });
+
+    setPoke(updatePokemon);
+    localStorage.setItem("pokemonList", JSON.stringify(updatePokemon));
+  }, []);
+
   return (
     <div>
       <Row
@@ -10,8 +32,8 @@ export default function PokemonList({ pokemons }) {
           marginTop: "70px",
         }}
       >
-        {pokemons &&
-          pokemons.map((pokemon, index) => (
+        {poke &&
+          poke.map((pokemon, index) => (
             <PokemonCard
               key={index}
               name={pokemon.name}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,8 +6,6 @@ import { GET_ALL_POKEMONS } from "../graphql/getAllPokemons";
 import Loader from "../components/pokemon-item/Loader";
 
 export default function HomePage() {
-  const [pokemons, setPokemons] = useState([]);
-
   const gqlVariables = {
     limit: 15,
     offset: 1,
@@ -17,33 +15,13 @@ export default function HomePage() {
     variables: gqlVariables,
   });
 
-  useEffect(() => {
-    let getPokemons;
-
-    if (JSON.parse(localStorage.getItem("pokemonList"))) {
-      getPokemons = JSON.parse(localStorage.getItem("pokemonList"));
-    } else {
-      getPokemons = data.pokemons.results || [];
-    }
-
-    const updatePokemon = getPokemons.map((poke) => {
-      return {
-        ...poke,
-        owned: poke.owned ? poke.owned : 0,
-      };
-    });
-
-    setPokemons(updatePokemon);
-    localStorage.setItem("pokemonList", JSON.stringify(updatePokemon));
-  }, []);
-
   if (loading) return <Loader />;
   if (error) return <p>Shit happened...</p>;
 
   return (
     <div>
       <Container>
-        <PokemonList pokemons={pokemons} />
+        <PokemonList pokemons={data.pokemons.results} />
       </Container>
     </div>
   );
